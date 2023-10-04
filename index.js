b0bc646e1b6ef6c596ded52fd2014def7c630586
Felipe Andrés Palma Álvarez <felipeandres_uc@outlook.com>
2023-10-04 14:24:36 -0300

    agregar colors e index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import {inquirerMenu, pausa, leerInput, listadoTareasBorrar, confirmar, mostrarListadoChecklist} from './helpers/inquirer.js'
+
+const main = async() => {
+    console.log("hola mundo")
+}
+
+main()
